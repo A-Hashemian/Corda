2de1c61f8d31e23ead443cbca89e2aa77fbe031b
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-07 23:55:40 +0300

    Update db_conn.java

--- Web/Hibernate/db_conn.java
+++ Web/Hibernate/db_conn.java
@@ -25,4 +25,9 @@ public class HibernateUtil {
       public static SessionFactory getSessionFactory() {
       return sessionFactory;
    }
+   
+     public static void shutdown() {
+      // Close caches and connection pools
+      getSessionFactory().close();
+   }
  }
