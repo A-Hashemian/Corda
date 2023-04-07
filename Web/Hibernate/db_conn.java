5d4072c96197705bd34dc521f6b5ba79018725ef
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-07 23:54:04 +0300

    Update db_conn.java

--- Web/Hibernate/db_conn.java
+++ Web/Hibernate/db_conn.java
@@ -1,1 +1,24 @@
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+   private static final SessionFactory sessionFactory = buildSessionFactory();
+
+   private static SessionFactory buildSessionFactory() {
+      try {
+         // Create the SessionFactory from hibernate.cfg.xml
+         Configuration configuration = new Configuration();
+         configuration.configure();
+         SessionFactory sessionFactory = configuration.buildSessionFactory();
+         return sessionFactory;
+      } catch (Throwable ex) {
+         // Make sure you log the exception, as it might be swallowed
+         System.err.println("Initial SessionFactory creation failed." + ex);
+         throw new ExceptionInInitializerError(ex);
+      }
+   }
+  
+ }
