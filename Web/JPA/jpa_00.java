241691a1e86fe4cef3f6396c7fed3d2db263c845
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-16 23:59:42 +0300

    Update jpa_00.java

--- Web/JPA/jpa_00.java
+++ Web/JPA/jpa_00.java
@@ -3,3 +3,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an EntityManagerFactory with the persistence unit name "myDatabase"
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
+        
+        // Create an EntityManager to interact with the database
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        // Begin a new transaction using the EntityTransaction object
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+      
+      
+    }
