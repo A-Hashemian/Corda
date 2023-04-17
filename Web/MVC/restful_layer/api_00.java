6e44ac353cf324fe4c440273983cea75a2e8a4c2
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-17 22:56:17 +0300

    Update api_00.java

--- Web/MVC/restful_layer/api_00.java
+++ Web/MVC/restful_layer/api_00.java
@@ -13,4 +13,19 @@ public class UserResource {
     
      // In-memory collection to store users
     private static Map<String, User> users = new HashMap<>();
+    
+      // GET method to retrieve a user by ID
+    @GET
+    @Path("/{userId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("userId") String userId) {
+        User user = users.get(userId);
+        if (user != null) {
+            return Response.ok(user).build(); // Return HTTP 200 OK with user object
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build(); // Return HTTP 404 Not Found if user doesn't exist
+        }
+    }
+    
+    
 }
