27f502cae54e5c5614301ec0fd6d8f1b9b12e31c
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-17 22:51:59 +0300

    Update api_00.java

--- Web/MVC/restful_layer/api_00.java
+++ Web/MVC/restful_layer/api_00.java
@@ -1,1 +1,14 @@
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+public class UserResource {
+
+    private static Map<String, User> users = new HashMap<>();
+}
