249aa37d68c57da96e40806e38ed881ce838194d
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-22 00:02:57 +0300

    Update Dep_Injection_00.java

--- Web/Spring_Core/DI/Dep_Injection_00.java
+++ Web/Spring_Core/DI/Dep_Injection_00.java
@@ -1,6 +1,6 @@
 
-//  here is an simple example Java code that demonstrates dependency injection using the Spring frameworks core module
 /*
+here is an simple example Java code that demonstrates dependency injection using the Spring frameworks core module
 First you need to define the bean to be injected
 Lets say you have a "UserRepository" class that implements a "UserDAO" interface
 you can define this bean in our Spring configuration file (lets call it "application-context.xml") as follows
