ea68fdaaffdbf05e150b04a5dd498e6a8c5411d1
A-Hashemian <amin@hashemian.tech>
2023-07-28 16:18:24 +0300

    db_connection

--- Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/connention/DBConn.java
+++ Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/connention/DBConn.java
@@ -1,7 +1,18 @@
 package shopingo.connention;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConn {
 	private static Connection connection = null;
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping_cart","root","pass");
+            System.out.print("connected");
+        }
+        return connection;
+    }
 }
