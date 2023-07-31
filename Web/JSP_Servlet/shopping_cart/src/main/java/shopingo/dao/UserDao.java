baf2d6d165969aed762fba062dda631343a807b4
A-Hashemian <amin@hashemian.tech>
2023-07-31 22:01:01 +0300

    dao

--- Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/dao/UserDao.java
+++ Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/dao/UserDao.java
@@ -1,5 +1,19 @@
 package shopingo.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDao {
+	
+	private Connection con;
+
+	private String query;
+    private PreparedStatement pst;
+    private ResultSet rs;
+
+	public UserDao(Connection con) {
+		this.con = con;
+	}
 
 }
