2fba236212b4050250d2a1d3ef810fe8ececa9d2
A-Hashemian <amin@hashemian.tech>
2023-07-31 22:25:44 +0300

    dao

--- Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/dao/UserDao.java
+++ Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/dao/UserDao.java
@@ -3,6 +3,9 @@ package shopingo.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import shopingo.model.User;
 
 public class UserDao {
 	
@@ -15,5 +18,25 @@ public class UserDao {
 	public UserDao(Connection con) {
 		this.con = con;
 	}
+	
+	public User userLogin(String email, String password) {
+		User user = null;
+        try {
+            query = "select * from users where email=? and password=?";
+            pst = this.con.prepareStatement(query);
+            pst.setString(1, email);
+            pst.setString(2, password);
+            rs = pst.executeQuery();
+            if(rs.next()){
+            	user = new User();
+            	user.setId(rs.getInt("id"));
+            	user.setName(rs.getString("name"));
+            	user.setEmail(rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            System.out.print(e.getMessage());
+        }
+        return user;
+    }
 
 }
