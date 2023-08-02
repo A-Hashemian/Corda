3afacbf5125f170f62f318032b9682e7604f82fb
A-Hashemian <amin@hashemian.tech>
2023-08-02 23:19:10 +0300

    logout_servlet

--- Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/servlet/LogoutServlet.java
+++ Web/JSP_Servlet/shopping_cart/src/main/java/shopingo/servlet/LogoutServlet.java
@@ -1,6 +1,8 @@
 package shopingo.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,26 +15,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/log-out")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
+	   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			if(request.getSession().getAttribute("auth")!=null) {
+				request.getSession().removeAttribute("auth");
+				response.sendRedirect("login.jsp");
+			}else {
+				response.sendRedirect("index.jsp");
+			}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+		} 
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
